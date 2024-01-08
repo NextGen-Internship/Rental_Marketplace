71e87c88c44ec246f53f50923b08cf8cd15ff8fc
Slavchev03 <biser.slavchev@blankfactor.com>
2024-01-08 22:23:21 +0200

    Added JwtAuthFilter,JwtService and dependecy for Jwt in pom file

---
+++ back-end/src/main/java/com/devminds/rentify/configuration/JwtService.java
@@ -0,0 +1,10 @@
+package com.devminds.rentify.configuration;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+    public String excractUsername(String jwt) {
+        return null;
+    }
+}
