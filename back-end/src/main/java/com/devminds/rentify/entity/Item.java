e8638dab6fbb276e03389bc25bef4a15a5941883
Slavchev03 <biser.slavchev@blankfactor.com>
2024-01-08 21:05:58 +0200

    Update Id in Item and User

--- back-end/src/main/java/com/devminds/rentify/entity/Item.java
+++ back-end/src/main/java/com/devminds/rentify/entity/Item.java
@@ -13,7 +13,7 @@ import java.util.List;
 @Entity
 @Table(name = "item")
 public class Item {
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Id
     private int id;
 
--- back-end/src/main/java/com/devminds/rentify/entity/User.java
+++ back-end/src/main/java/com/devminds/rentify/entity/User.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Entity
 @Table(name = "user")
 public class User {
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Id
     private int id;
 
