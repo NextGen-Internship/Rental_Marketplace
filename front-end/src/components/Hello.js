50c3e622588a1c2fba4f2eb68b2483897987e8a2
Maria Markova <maria.markova@blankfactor.com>
2024-01-08 11:35:54 +0200

    Create simple Hello controller. Create simple get request with axios on frontend.

---
+++ front-end/src/components/Hello.js
@@ -0,0 +1,29 @@
+import { fetchData } from "./fetchData"
+import React, { useEffect, useState } from 'react';
+
+const endpoint = 'hello';
+
+const Hello = () => {
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+      const fetchDataFromApi = async () => {
+        try {
+          const result = await fetchData(endpoint);
+          setData(result);
+        } catch (error) {
+          // Handle error
+        }
+      };
+  
+      fetchDataFromApi();
+    }, []);
+  
+    return (
+      <div>
+        <h1>Hello, World!</h1>
+      </div>
+    );
+}
+
+export default Hello
---
+++ front-end/src/components/fetchData.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const apiUrl = 'http://localhost:8080/rentify';
+
+export const fetchData = async (endpoint) => {
+  try {
+    const response = await axios.get(`${apiUrl}/${endpoint}`);
+    console.log(response);
+    
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    throw error;
+  }
+};
