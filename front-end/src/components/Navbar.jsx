23741e946c41397aaa5a594994bf473e45513798
Maria Markova <maria.markova@blankfactor.com>
2024-01-10 11:37:51 +0200

    Change settings button to user button.

--- front-end/src/components/Navbar.jsx
+++ front-end/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 import logo from '../assets/rentify-logo.svg'
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder'; 
 import LogoutIcon from '@mui/icons-material/Logout';
-import SettingsIcon from '@mui/icons-material/Settings';
+import PersonIcon from '@mui/icons-material/Person';
 
 const Navbar = () => {
   return (
@@ -13,7 +13,7 @@ const Navbar = () => {
             <Link to="/">Home</Link>
             <Link to="/create">Add Item</Link>
             <Link to="/Likes"> <FavoriteBorderIcon /> </Link>
-            <Link to="/Settings"> <SettingsIcon /> </Link>
+            <Link to="/Settings"> <PersonIcon /> </Link>
             <Link to="/Logout"> <LogoutIcon /> </Link>
         </div>
     </nav> 
