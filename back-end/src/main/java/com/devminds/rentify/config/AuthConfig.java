7f0e9bb1790f00162792123f4f64338ec568cfec
Maria Markova <maria.markova@blankfactor.com>
2024-01-18 11:39:49 +0200

    S3 setup.

--- back-end/src/main/java/com/devminds/rentify/config/AuthConfig.java
+++ back-end/src/main/java/com/devminds/rentify/config/AuthConfig.java
@@ -2,8 +2,10 @@ package com.devminds.rentify.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -11,13 +13,17 @@ import org.springframework.security.web.SecurityFilterChain;
 public class AuthConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(auth -> auth
-                .requestMatchers("/rentify/categories").permitAll()
-                .requestMatchers("/rentify/categories/*").permitAll()
-                .requestMatchers("/rentify/items").permitAll()
-                .requestMatchers("/rentify/items/*").permitAll()
-//                .requestMatchers("/h2/**").permitAll()
-                .anyRequest().authenticated());
+//        http.authorizeHttpRequests(auth -> auth
+//                .requestMatchers("/rentify/categories").permitAll()
+//                .requestMatchers("/rentify/categories/*").permitAll()
+//                .requestMatchers("/rentify/items").permitAll()
+//                .requestMatchers("/rentify/items/*").permitAll()
+//                .requestMatchers("/file/upload").permitAll()
+////                .requestMatchers("/h2/**").permitAll()
+//                .anyRequest().authenticated());
+//        return http.build();
+        http.authorizeHttpRequests(authorizeRequests -> authorizeRequests.requestMatchers(HttpMethod.POST, "/file/upload").permitAll().anyRequest()
+                .permitAll());
         return http.build();
     }
 }
