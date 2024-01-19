c72ff06fdc4f67ef9f69c495dcfab77f802abf31
ValentinTsonevskii <152274384+ValentinTsonevskii@users.noreply.github.com>
2024-01-19 16:40:20 +0200

    bugfix
    roles are fixed

--- back-end/src/main/java/com/devminds/rentify/repository/RoleRepository.java
+++ back-end/src/main/java/com/devminds/rentify/repository/RoleRepository.java
@@ -2,8 +2,14 @@ package com.devminds.rentify.repository;
 
 import com.devminds.rentify.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Integer> {
+
+    @Query("SELECT u FROM Role u WHERE u.id = 2")
+    Optional<Role> findUserRole();
 }
