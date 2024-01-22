aeeff909ad75125962f1967de49d8619f8902222
maria-markova <152273838+maria-markova@users.noreply.github.com>
2024-01-22 12:57:11 +0200

    Update navbar. Delete tokens from localStorage when logging out.

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -8,19 +8,34 @@ import CategoryDetails from './components/categories/CategoryDetails';
 import ItemDetails from './components/items/ItemDetails';
 import Login from "./components/login/Login";
 import Register from "./components/login/Register";
+import { useState } from 'react';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(!!localStorage.getItem('google_token') || !!localStorage.getItem('token'));
+
+  const handleLogin = () => {
+    localStorage.setItem('google_token', localStorage.getItem('google_token')); 
+    //localStorage.setItem('token', localStorage.getItem('token')); 
+    setIsLoggedIn(true);
+  };
+
+  const handleLogout = () => {
+    localStorage.removeItem('google_token');
+    localStorage.removeItem('token');
+    setIsLoggedIn(false);
+  };
+
   return (
     <Router>
       <div className="App">
-        <Navbar />
-        
+      <Navbar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} onLogin={handleLogin} onLogout={handleLogout} />
+
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route path="/items/category/:id" element={<CategoryDetails />} />
           <Route path="items/:id" element={<ItemDetails />} />
-           <Route exact path="/login" element={<Login />} />
-            <Route exact path="/register" element={<Register />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/register" element={<Register />} />
           <Route path="*" element={<NotFound />} />
           <Route path="/notfound" element={<NotFound />} />
         </Routes>
--- front-end/src/components/navbar/Navbar.jsx
+++ front-end/src/components/navbar/Navbar.jsx
@@ -1,24 +1,44 @@
 import { Link } from 'react-router-dom';
-import logo from '../../assets/rentify-logo.svg'
-import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder'; 
+import { useEffect } from 'react';
+import logo from '../../assets/rentify-logo.svg';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import LogoutIcon from '@mui/icons-material/Logout';
 import PersonIcon from '@mui/icons-material/Person';
-import "./Navbar.css"
+import "./Navbar.css";
 
-const Navbar = () => {
+const Navbar = ({ isLoggedIn, setIsLoggedIn, onLogin, onLogout }) => {
+  useEffect(() => {
+    const googleToken = localStorage.getItem('google_token');
+    setIsLoggedIn(!!googleToken);
+  }, [isLoggedIn]);
+
+  const handleLogout = () => {
+    Object.keys(localStorage).forEach(key => { localStorage.removeItem(key); });
+    setIsLoggedIn(false);
+    onLogout();
+  };
+
   return (
     <nav className="navbar">
-        <Link to="/"> <img src={logo} width={"200px"}/> </Link>
-
-        <div className="links">
-            <Link to="/">Home</Link>
-            <Link to="/create">Add Item</Link>
+      <Link to="/"> <img src={logo} width={"200px"} /> </Link>
+      <div className="links">
+        <Link to="/">Home</Link>
+        <Link to="/create">Add Item</Link>
+        {isLoggedIn ? (
+          <>
             <Link to="/likes"> <FavoriteBorderIcon /> </Link>
             <Link to="/settings"> <PersonIcon /> </Link>
-            <Link to="/logout"> <LogoutIcon /> </Link>
-        </div>
-    </nav> 
-  )
+            <Link to="/" onClick={handleLogout}> <LogoutIcon /> </Link>
+          </>
+        ) : (
+          <>
+            <Link to="/login" onClick={onLogin}>Login</Link>
+            <Link to="/register">Register</Link>
+          </>
+        )}
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
