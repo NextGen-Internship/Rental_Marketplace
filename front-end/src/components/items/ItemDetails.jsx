1316ddeafe09c5a5b7b90e4bcddff01073f0a94f
maria-markova <152273838+maria-markova@users.noreply.github.com>
2024-01-23 12:31:49 +0200

    Add ItemDetails component.

--- front-end/src/components/items/ItemDetails.jsx
+++ front-end/src/components/items/ItemDetails.jsx
@@ -1,8 +1,34 @@
+import React, { useState, useEffect } from 'react';
+import { fetchData } from '../fetchData';
+import { useParams, useNavigate } from 'react-router-dom';
+
+const endpoint = 'items/';
 
 const ItemDetails = () => {
+  const [item, setItem] = useState(null);
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchItem = async () => {
+      try {
+        const result = await fetchData(endpoint + id);
+        console.log(result);
+
+        setItem(result);
+      } catch (error) {
+        navigate("/notfound");
+      }
+    };
+
+    if (!item) {
+      fetchItem();
+    }
+  }, [item, id, navigate]);
+
   return (
     <div>ItemDetails</div>
-  )
-}
+  );
+};
 
-export default ItemDetails
+export default ItemDetails;
