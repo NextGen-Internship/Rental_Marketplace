99311f378e2841060c3a0a4647871de4123a84a3
ValentinTsonevskii <152274384+ValentinTsonevskii@users.noreply.github.com>
2024-02-01 16:59:46 +0200

    create stripe customer

--- back-end/src/main/java/com/devminds/rentify/controller/PaymentController.java
+++ back-end/src/main/java/com/devminds/rentify/controller/PaymentController.java
@@ -3,8 +3,10 @@ package com.devminds.rentify.controller;
 import com.devminds.rentify.dto.CreatePayment;
 import com.devminds.rentify.dto.CreatePaymentResponse;
 import com.stripe.exception.StripeException;
+import com.stripe.model.Customer;
 import com.stripe.model.PaymentIntent;
 import com.stripe.param.AccountCreateParams;
+import com.stripe.param.CustomerCreateParams;
 import com.stripe.param.PaymentIntentCreateParams;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,14 +19,22 @@ public class PaymentController {
     @PostMapping("/create-payment-intent")
     public CreatePaymentResponse createPaymentIntent(@RequestBody CreatePayment createPayment) throws StripeException {
 
-        AccountCreateParams accountCreateParams = new AccountCreateParams.Builder()
-                .setEmail(createPayment.getCustomer().getEmail())
+//        AccountCreateParams accountCreateParams = new AccountCreateParams.Builder()
+//                .setEmail(createPayment.getCustomer().getEmail())
+//                .build();
 
-                .build();
+        CustomerCreateParams params =
+                CustomerCreateParams.builder()
+                        .setName("Jenny Rosen")
+                        .setEmail("jennyrosen@example.com")
+                        .build();
+        Customer customer = Customer.create(params);
+
 
         PaymentIntentCreateParams createParams = new PaymentIntentCreateParams.Builder()
                 .setCurrency("usd")
                 .setAmount(createPayment.calculateOrderAmount() * 100L)
+                .setCustomer(customer.getId())
                 .build();
 
         PaymentIntent paymentIntent = PaymentIntent.create(createParams);
